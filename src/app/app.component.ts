e29775d3b8dedadf5362b4f930d01ab4aa5c8c15
kainasaru <kainasaru@gmail.com>
2022-08-31 18:56:51 -0300

    cambios en titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Leonardo Ariel Manassali - LAB 4 2022';
   num1 = 0;
   num2 = 0;
   suma = 0;
