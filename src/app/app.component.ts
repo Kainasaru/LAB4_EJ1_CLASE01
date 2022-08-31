0cd21bde4e092c078225bc267ea5c9449c6a8b74
kainasaru <kainasaru@gmail.com>
2022-08-31 17:22:31 -0300

    Primer commit. Modificaciones para deploy en heroku

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ej1Clase01';
+  num1 = 0;
+  num2 = 0;
+  suma = 0;
+  promedio = 0;
+
+  Limpiar() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.suma = 0;
+    this.promedio = 0;
+  }
+
+  Calcular(){
+    this.suma = this.num1 + this.num2;
+    this.promedio = this.suma / 2;
+  }
+}
+
