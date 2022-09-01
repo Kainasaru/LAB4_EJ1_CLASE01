7bf6724e6668d80ecb14276e80050f431f2372c0
kainasaru <kainasaru@gmail.com>
2022-09-01 09:40:38 -0300

    Eliminados comps del  ejercicio 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BienvenidoComponent } from './bienvenido/bienvenido.component';
-import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BienvenidoComponent,
-    LoginComponent,
-    ErrorComponent
   ],
   imports: [
     BrowserModule,
